e6783bc2b3598860873dece0215ce2e5055a53f0
alextaves <42424246+alextaves@users.noreply.github.com>
2019-02-06 18:42:09 -0500

    image_styling
    some styling that still may need some attention

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import React from "react";
 
 const App = () => (
-  <div className="images">
-    <div className="image">
-      <img src={require("./01 small.png")} />
+  <div className="container">
+    <div className="images">
+      <div className="image">
+        <img src={require("./01 small.png")} />
+        <h3>alex</h3>
+        <p className="paragraph">artist: oswin blue</p>
+        <p className="paragraph">price: $1900</p>
+      </div>
+
+      <div className="image">
+        <img src={require("./02 small.png")} />
+        <h3>bob</h3>
+        <p className="paragraph">artist: oswin blue</p>
+        <p className="paragraph">price: $2100</p>
+      </div>
+
+      <div className="image">
+        <img src={require("./03 small.png")} />
+        <h3>dave</h3>
+        <p className="paragraph">artist: oswin blue</p>
+        <p className="paragraph">price: $1500</p>
+      </div>
+
+      <div className="image">
+        <img src={require("./04 small.png")} />
+        <h3>karl</h3>
+        <p className="paragraph">artist: oswin blue</p>
+        <p className="paragraph">price: $1200</p>
+      </div>
     </div>
   </div>
 );
